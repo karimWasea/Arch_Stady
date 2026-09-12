e25d7982ad70b9d1175712d1ada2b19d826afd02
kareemnaser95 <karim.naser@techeffic.com>
2026-09-13 02:11:47 +0300

    Hexagonal Architecture (Ports & Adapters)

--- HospitalManagement.API/Program.cs
+++ HospitalManagement.API/Program.cs
@@ -122,7 +122,7 @@ builder.Services.AddSwaggerGen(options =>
         Scheme = "Bearer"
     });
 
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    options.AddSecurityRequirement(new  OpenApiSecurityRequirement
     {
         {
             new OpenApiSecurityScheme
